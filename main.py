b0ac971b96ab927bdca5f498981b4458aa499dac
QZ_Map <QZ_Map@proton.me>
2023-02-14 16:02:52 +0100

    populated setup

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 def setup():
     # starts shutdown timer
     # calibrations???
+    wait4light()
+    KIPR.shut_down_in(180)
     return
 
-def wait4light()
+def wait4light():
     """ only continuies when starting light has been aktivated
     """
-    light = False #check 4 lght pls
-    while not light:
-        light = False
-    return
+    port = 7 ## random port
+    KIPR.wait_for_light(port)
+    # light = False #check 4 lght pls
+    # while not light:
+    #     light = False
+    # return
 
 setup()
-wait4light()
-
