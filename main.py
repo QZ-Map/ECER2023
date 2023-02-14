2b06a7d5b556a3e4cf37fd2bbc8a9f1d681af86c
QZ_Map <QZ_Map@proton.me>
2023-02-14 15:50:31 +0100

    populate reposetory

---
+++ main.py
@@ -0,0 +1,16 @@
+def setup():
+    # starts shutdown timer
+    # calibrations???
+    return
+
+def wait4light()
+    """ only continuies when starting light has been aktivated
+    """
+    light = False #check 4 lght pls
+    while not light:
+        light = False
+    return
+
+setup()
+wait4light()
+
