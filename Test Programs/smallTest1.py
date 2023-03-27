81c377f90c033a2a18b6247dbcc31b1962345e56
Andreas Patuzzi <40146720190105@litec.ac.at>
2023-03-27 10:50:59 +0200

    small robot red pickup

--- Test Programs/smallTest1.py
+++ Test Programs/smallTest1.py
@@ -62,9 +62,7 @@ class Motor:
     def alloff():
         """Stops all motors on the robot and releases their current positions."""
         KIPR.alloff()
-        
-     
-     
+
      
         
 class Wheels: 
@@ -169,11 +167,260 @@ class Wheels:
         rotationDistance = self.distanceToTicks((self.rotarionU * angle) / 360)
         self.motorRight.mrp(speed, -int(rotationDistance))
         self.motorLeft.mrp(speed, int(rotationDistance))
+  
+  
+  
+class Analog:
+    """
+    This class provides an interface for reading analog sensor values.
+    """
+    def __init__(self, port):
+        self.port = port
+                
+    def __str__(self):
+        return str(self.analog())
+    
+
+    def analog(self):
+        """
+        Gets the 12-bit analog value of the sensor connected to the specified port.
+
+        Returns:
+            The latest 12-bit value of the port (a value in the range 0 to 4095).
+        """
+        return KIPR.analog(self.port)
+
+    def analog10(self):
+        """
+        Gets the 10-bit analog value of the sensor connected to the specified port.
+
+        Returns:
+            The latest 10-bit value of the port (a value in the range 0 to 1023).
+        """
+        return KIPR.analog10(self.port)
+
+    def analog8(self):
+        """
+        Gets the 8-bit analog value of the sensor connected to the specified port.
+
+        Returns:
+            The latest 8-bit value of the port (a value in the range 0 to 255).
+        """
+        return KIPR.analog8(self.port)
+
+    def analog_et(self):
+        """
+        Gets the 10-bit analog value of the ET sensor connected to the specified port.
+
+        Returns:
+            The latest 10-bit value of the port (a value in the range 0 to 1023).
+        """
+        return KIPR.analog_et(self.port)
+
+    def set_pullup(self, pullup):
+        """
+        Sets the analog pullup status for the sensor connected to the specified port.
+
+        Args:
+            pullup: A value of 0 (inactive) or 1 (active).
+        """
+        KIPR.set_analog_pullup(self.port, pullup)
+
+    def get_pullup(self):
+        """
+        Gets the analog pullup status for the sensor connected to the specified port.
+
+        Returns:
+            The status of the analog pullup on the specified port.
+        """
+        return KIPR.get_analog_pullup(self.port)
+    
+    
+    @staticmethod
+    def analog_(port):
+        return KIPR.analog(port)
+    
+    @staticmethod
+    def analog10_(port):
+        return KIPR.analog10(port)
+    
+    @staticmethod
+    def analog8_(port):
+        return KIPR.analog8(port)
+    
+    @staticmethod
+    def analog_et_(port):
+        return KIPR.analog_et(port)
+    
+    @staticmethod
+    def set_pullup_(port, pullup):
+        KIPR.set_analog_pullup(port, pullup)
         
+    @staticmethod
+    def get_pullup_(port):
+        return KIPR.get_analog_pullup(port)      
+
+
+
+class Servo:
+    def __init__(self, port):
+        self.port = port
+                
+    def __str__(self):
+        return str(self.get_position())
+    
+    
+    def enable(self):
+        """
+        Enable a specific servo.
+        """
+        KIPR.enable_servo(self.port)
+        
+    def disable(self):
+        """
+        Disable a specific servo.
+        """
+        KIPR.disable_servo(self.port)
+    
+    def set_enabled(self, enabled):
+        """
+        Enable or disable a specific servo.
+
+        Parameters:
+        - enabled: The new enable setting 0: disabled 1: enabled
+        """
+        KIPR.set_servo_enabled(self.port, enabled)
+        
+    def get_enabled(self):
+        """
+        Check if a servo is enabled.
+
+        Returns:
+        - The servo enable setting 0: disabled 1: enabled
+        """
+        return KIPR.get_servo_enabled(self.port)
+    
+    def get_position(self):
+        """
+        Get the most recent commanded servo position.
+
+        Returns:
+        - The servo's position as an 11 bit integer (which is an integer between 0 and 2047)
+        """
+        return KIPR.get_servo_position(self.port)
+    
+    def set_position(self, position):
+        """
+        Set a new servo goal position.
+
+        Parameters:
+        - position: The new servo position, between 0 and 2047
+
+        Note:
+        - Even though the servos have a theoretical range between 0 and 2047, the actual range is often less. Setting the servo to a position that it cannot physically reach will cause the servo to audibly strain and will consume battery very quickly.
+        """
+        KIPR.set_servo_position(self.port, position)        
+        
+    def timed_movement(self, position, time = 1000):
+        """ Move the servo to a specific position in a specified total time in ms """
+        currentPos = self.get_position()
+        startPos = currentPos
+        stepSize = 0
+        stepTime = 0
+        while stepTime == 0:
+            stepSize += (1 if (position>currentPos) else -1)
+            stepTime = int(time / ((position-currentPos)/stepSize))
+        substepCount = (position-currentPos) // stepSize
+        #print(stepTime, stepSize, substepCount)
+        for x in range(1, substepCount+1):
+            #print("move to: ", (startPos+stepSize*x), ", wait: ", stepTime)
+            self.set_position(startPos + stepSize*x)
+            KIPR.msleep(stepTime)
+        #print("move to: ", position)
+        self.set_position(position)
+    
+        
+    @staticmethod
+    def enable_all():
+        """
+        Enable all four servo channels.
+        """
+        KIPR.enable_servos()
+    
+    @staticmethod
+    def disable_all():
+        """
+        Disable all four servo channels.
+        """
+        KIPR.disable_servos()
+
+
+
 
 
 wheels = Wheels(0, 1, 69, 165)
-wheels.standRotate(180, 1000)
+servoHeight = Servo(0)
+servoHead = Servo(1)
+servoHeight.enable_all()
+
+KIPR.msleep(1000) 
+
+servoHeight.set_position(1530)
+servoHead.set_position(0)
+
+wheels.mrp(1000, 1900)
 while not wheels.gmd():
-    KIPR.msleep(10)    
+    KIPR.msleep(10)  
+wheels.mrp(1000, 1000)
+
+wheels.standRotate(115, 1000)
+while not wheels.gmd():
+    KIPR.msleep(10)  
+    
+wheels.mrp(1000, 4000)
+while not wheels.gmd():
+    KIPR.msleep(10) 
+    
+        
+while Analog.analog_(0) < 2500:
+    wheels.mrp(1000, 50)
+    KIPR.msleep(10)  
+    
+wheels.mrp(500, 350)
+while not wheels.gmd():
+    KIPR.msleep(10) 
+
+
+servoHead.timed_movement(700, 500)
+servoHeight.timed_movement(1480, 100)
+
+wheels.mrp(1000, -2000)
+while not wheels.gmd():
+    KIPR.msleep(10)  
+
+servoHeight.timed_movement(300, 1000)   #lift cube
+
+wheels.standRotate(160, 500)
+while not wheels.gmd():
+    KIPR.msleep(10) 
+    
+    
+wheels.mrp(1000, 12000)
+while not wheels.gmd():
+    KIPR.msleep(10) 
+    
+while Analog.analog_(0) < 2000:
+    wheels.mrp(1000, 50)
+    KIPR.msleep(10) 
+    
+wheels.mrp(1000, -400)
+while not wheels.gmd():
+    KIPR.msleep(10) 
+    
+#place cube
+servoHeight.timed_movement(1480, 1000)
+servoHead.timed_movement(0, 500)
+servoHeight.timed_movement(300, 500)
+
 print("finished")
+servoHead.set_position(0)
